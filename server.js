c63b3dad48a8c1c7d34c25f3ef3f91750c45d7b0
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-15 11:35:13 +0000

    [Creating Routes And responses In Express]
    The following additions define routes for the application. These routes include GET requests to /api/v1/bootcamps, /api/v1/bootcamps/:id, PUT requests to /api/v1/bootcamps/:id, POST requests to /api/v1/bootcamps, and DELETE requests to /api/v1/bootcamps/:id.
    
    The response for each route is a JSON object with a success field indicating whether the request was successful, and a msg field containing a message.

--- server.js
+++ server.js
@@ -3,10 +3,33 @@ const dotenv = require('dotenv');
 
 //Load environment variables
 dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 5000;
 
+//Initialize Express
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//Routes
+//Get all bootcamps
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps.' });
+});
+//Get a single bootcamp
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Show one bootcamp ${req.params.id}.` });
+});
+//Update a bootcamp
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}.` });
+});
+//Create a bootcamp
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create a bootcamp.' });
+});
+//Delete a bootcamp
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}.` });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...Happy Hacking!!`);
 });
