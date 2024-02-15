c9a17dbcd0cf91a84194d8b690b825c3ac1a650a
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-15 22:49:26 +0000

    [Third party middleware for logging: Morgan]
    npm install morgan
    
    [server.js]
    import/require morgan.
    
    add a middleware function that is used to enable logging in the development environment. The morgan package is used to log requests to the console in a specific format. In this case, the dev option is used, which logs the request method, URL, status code, response time, and the request body.
    
    This middleware is only applied in the development environment, which can be determined by checking the NODE_ENV variable. If the value of NODE_ENV is development, the middleware is applied, otherwise it is not. This allows for different logging configurations depending on the environment.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger.js');
+const morgan = require('morgan');
 
 //Route Files
 const bootcamps = require('./routes/bootcamps.js');
@@ -12,8 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-//use logger middleware
-app.use(logger);
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Routes
 //Mount Routers
