934219aa5fbc6b3bae48857ae0808443f8e0d1e2
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-16 01:08:19 +0000

    [Connecting to mongoDB] install mongoose.
    [Create db.js]
    
    Here we create a Nodejs module that exports a function called connectDB. The function uses the mongoose library to connect to a MongoDB database. The MONGO_URI environment variable is used to store the connection string.
    
    The code starts by importing the mongoose library. Then, the connectDB function is defined. It uses the async keyword to make the function asynchronous.
    
    The connectDB function uses the mongoose.connect method to connect to the MongoDB database. The process.env.MONGO_URI variable is used to get the connection string from the environment.
    
    Once the connection is established, the code logs a message to the console indicating that the MongoDB connection was successful.
    
    Next we import the module to server.js and make a call to the function in db.js to connect to the database.
    
    Next we change the server code so that we can mannage unhandled exceptions. here:
    
    On line 28, the app.listen() method is now placed within the variable server and is used to start the server and listen for requests on the specified port. The callback function logs a message to the console indicating that the server is running and provides the port number.
    
    On line 32, the process.on() method is used to listen for uncaught exceptions. If an exception is thrown and not handled, the callback function will log the error message to the console and then close the server and exit the process with a status code of 1. This will cause the Node.js process to terminate and any active connections to be closed.
    We can now connect to our database. And exit if we have unhandled exceptions.
    Happy hacking!!

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger.js');
 const morgan = require('morgan');
-
-//Route Files
-const bootcamps = require('./routes/bootcamps.js');
+const connectDB = require('./config/db');
 
 //Load environment vars
 dotenv.config({ path: './config/config.env' });
+
+//Connect to MongoDB
+connectDB();
+
+//Route Files
+const bootcamps = require('./routes/bootcamps.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -22,6 +25,14 @@ if (process.env.NODE_ENV === 'development') {
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () => {
+//Start the server
+const server = app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...Happy Hacking!!`);
+});
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
 });
